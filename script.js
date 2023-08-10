0d0c65d90d27e05e3902b072651272f2317fd631
Yega Cho <yega1009@gmail.com>
2023-08-10 16:24:58 -0400

    added code to create time blocks from 9am to 5pm

--- script.js
+++ script.js
@@ -11,6 +11,16 @@ $(function () {
   var currentDay = dayjs().format('MMM D, YYYY');
   $('#currentDay').text(currentDay);
 
+  for (var i = 9; i <= 17; i++) {
+    var id = i - 9;
+    var row = $('</div>');
+    row.addClass('row');
+    row.addClass('time-block');
+    row.attr('id', 'hour-'+i);
+
+    $('.container-fluid').append(row);
+  }
+  
   // TODO: Add code to apply the past, present, or future class to each time
   // block by comparing the id to the current hour. HINTS: How can the id
   // attribute of each time-block be used to conditionally add or remove the
